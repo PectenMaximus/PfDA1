f49f885c7df281f66cf5a6a51eccd30cc687e72c
Alec Reid <muckrossreid@gmail.com>
2023-12-09 22:42:02 +0000

    Isolated Date for Moon Phase WIP3

--- PfDA1_Python_Code.py
+++ PfDA1_Python_Code.py
@@ -12,23 +12,51 @@ SpatProduction = ["High", "Medium","Low"]
 # Generating Random Temperatures
 TempRange = np.random.default_rng(12345)
 Temp = TempRange.integers(low=7 , high=18, size=100)
-print (Temp)
 
 # Generating Random Salinities
 SalinityRange = np.random.default_rng(12345)
 Sal = SalinityRange.integers(low=10 , high=35, size=100)
-print (Sal)
 
 # Generating Random Moonphase
 MPRange = np.random.choice(MoonPhase, size=100)
-print (MPRange)
 
 # Generating Random SpatProduction
 SpatRange = np.random.choice(SpatProduction, size=100)
-print (SpatRange)
 
-plt.bar(["Temperature","Salinity"],[Temp[0],Sal[0]])
-plt.title("SpatProduction")
-plt.xlabel("Events")
-plt.ylabel("TempValue")
+#Print the Full Range for all results
+for iLoop in range (0, 99):
+    print(Temp[iLoop],Sal[iLoop],MPRange[iLoop],SpatRange[iLoop])
+
+# Temperature against Salinty for Full Moon - Spat Production High=Green, Medium=Orange, Low=red.
+MasterArray = np.array []
+for iLoop in range (0,99):
+    if (MPRange[iLoop]=="Full"):
+        print(Temp[iLoop],Sal[iLoop],MPRange[iLoop],SpatRange[iLoop])
+        FullMoonTemp = (Temp[iLoop])
+        FullMoonSal = (Sal[iLoop])
+        MasterArray = [Temp[iLoop],Sal[iLoop]]
+
+# Temperature against Salinty for Half Moon - Spat Production High=Green, Medium=Orange, Low=red.
+for iLoop in range (0,99):
+    if (MPRange[iLoop]=="Half"):
+        print(Temp[iLoop],Sal[iLoop],MPRange[iLoop],SpatRange[iLoop])
+
+# Temperature against Salinty for Quarter Moon - Spat Production High=Green, Medium=Orange, Low=red.
+for iLoop in range (0,99):
+    if (MPRange[iLoop]=="Quarter"):
+        print(Temp[iLoop],Sal[iLoop],MPRange[iLoop],SpatRange[iLoop])
+         
+
+#plt.bar(["T1","S1"],[Temp[0],Sal[0]])
+#plt.title("SpatProduction")
+#plt.xlabel("Events")
+#plt.ylabel("TempValue")
+#plt.show()
+
+# Temperature against Salinity   
+plt.scatter([Temp],[Sal])  
+plt.axis((1,25,0,40))
+plt.xlabel("Temperature")
+plt.ylabel("Salinity")
+plt.xscale("linear")
 plt.show()
